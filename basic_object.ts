125fe5d19049617bdafca36a04fed94b163d4eaa
Shin Leonhardt <shinleonhardt19@gmail.com>
2025-08-10 15:10:07 +0700

    Add basic_objects/variable: my_wife as a new object

--- basic_object.ts
+++ basic_object.ts
@@ -12,7 +12,7 @@ function my_waifu_object(waifu_object: {name: string, age: number, husband:strin
     return waifu_properties;
 };
 
-function ultimate_waifu_object( waifu_object:{name: string, age: number, fav_food: string, fav_drink: string, fav_spec: string})
+function ultimate_waifu_object( waifu_object:{name: string, age: number | string, fav_food: string, fav_drink: string, fav_spec: string})
 {
     let waifus_properties = 
     {
@@ -27,5 +27,20 @@ function ultimate_waifu_object( waifu_object:{name: string, age: number, fav_foo
 
 let waifu = {name: "Bronya", age: 19, fav_food: "Moon cake", fav_drink: "cocktail", fav_spec: "hacker"}
 
-ultimate_waifu_object(waifu); // This is the dumpest object I've ever think of, I just test the function with dynamic objects :<
-console.log(`My wife favorite food is ${waifu.fav_food}`)
+let my_wife = {
+    name: "Bronya",
+    age: 19,
+    favorite_food: {
+        cooked_meal: ["Beef stew", "Steak", "Lasagna"],
+        drink: ["Cocktail", "Champange", "Lemon soda"],
+        desert: ["Tiramissu", "Mousse", "Souffle"]
+    },
+    job: "Hacker"
+}
+
+// Print my wife specific properties:
+
+console.log("My wife favorite food is:",my_wife.favorite_food.cooked_meal[1])
+
+// ultimate_waifu_object(waifu); // This is the dumpest object I've ever think of, I just test the function with dynamic objects :<
+// console.log(`My wife favorite food is ${waifu.fav_food}`)
