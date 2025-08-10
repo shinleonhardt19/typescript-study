da19dc21328b5ed0bb01ebb535a3a1aba8b11c69
Shin Leonhardt <shinleonhardt19@gmail.com>
2025-08-10 14:58:19 +0700

    Rewrite basic_objects/ultimate_waifu_object() to simple readable code :D

--- basic_object.ts
+++ basic_object.ts
@@ -12,29 +12,20 @@ function my_waifu_object(waifu_object: {name: string, age: number, husband:strin
     return waifu_properties;
 };
 
-
-function ultimate_waifu_object(waifu_object: {name:{name_1:string, name_2:string, name_3:string};age: number;
-                                             favorite_food:{food_1: string, food_2:string, food_3:string};
-                                             favorite_drink:{drink_1:string, drink_2:string, drink_3:string};
-                                             specify:{job_1:string, job_2:string, job_3:string}})
+function ultimate_waifu_object( waifu_object:{name: string, age: number, fav_food: string, fav_drink: string, fav_spec: string})
 {
     let waifus_properties = 
     {
         waifu_names: waifu_object.name,
-        waifu_food: waifu_object.favorite_food,
-        waifu_drink: waifu_object.favorite_drink,
-        waifu_specify: waifu_object.specify
+        waifu_food: waifu_object.fav_food,
+        waifu_drink: waifu_object.fav_drink,
+        waifu_specify: waifu_object.fav_spec
     }
-    console.log((waifus_properties))
+    console.log("My wife properties:", waifus_properties)
     return waifus_properties
 }
 
-let waifu = {
-    name: { name_1: "Aigis", name_2: "2B", name_3: "Bronya" },
-    age: 18,
-    favorite_food: { food_1: "Ramen", food_2: "Sushi", food_3: "Cake" },
-    favorite_drink: { drink_1: "Tea", drink_2: "Coffee", drink_3: "Juice" },
-    specify: { job_1: "Shadow executioner", job_2: "Fighter", job_3: "Hacker" }
-}
+let waifu = {name: "Bronya", age: 19, fav_food: "Moon cake", fav_drink: "cocktail", fav_spec: "hacker"}
 
 ultimate_waifu_object(waifu); // This is the dumpest object I've ever think of, I just test the function with dynamic objects :<
+console.log(`My wife favorite food is ${waifu.fav_food}`)
